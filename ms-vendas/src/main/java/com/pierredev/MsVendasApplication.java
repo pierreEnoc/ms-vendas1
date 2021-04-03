db69cd274eecc12cd39eb80c08b9a9adcf2743b8
Pierre enoc <pierre.enoc@gympass.com>
2021-04-03 18:39:38 -0300

    start project

---
+++ ms-vendas/src/main/java/com/pierredev/MsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.pierredev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVendasApplication.class, args);
+	}
+
+}
---
+++ ms-vendas/src/test/java/com/pierredev/MsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierredev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
